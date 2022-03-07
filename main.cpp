ac0bf53a5f6cab219f86e7560abdce68a96dac4e
ash14545 <russellfar@hotmail.com>
2022-03-07 00:04:38 -0600

    print inputs, k, and word count

--- main.cpp
+++ main.cpp
@@ -1,20 +1,42 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-    cout << "This program has " << argc << " arguments:" << endl;
-    for (int i = 0; i < argc; ++i)
+    int k = 0;
+
+    cout << endl;
+    cout << "Input Matrix (" << argv[1] << "):" << endl;
+    ifstream mFile;
+    mFile.open(argv[1]);
+    for (string line; getline(mFile, line);)
     {
-        cout << argv[i] << endl;
-
-        ifstream infile;
-        infile.open(argv[i]);
-
-        
+        k += 1;
+        cout << line << endl;
     }
+    mFile.close();
+
+    cout << endl;
+
+    cout << "Input Words (" << argv[2] << "):" << endl;
+    int wCount = 0;
+    ifstream wFile;
+    wFile.open(argv[2]);
+    for (string line; getline(wFile, line);)
+    {
+        wCount += 1;
+        cout << line << endl;
+    }
+    wFile.close();
+    cout << endl;
+
+    int matrix;
+    string words;
+    cout << "k: " << k << endl;
+    cout << "Total number of words: " << wCount << endl;
 
     return 0;
 }
